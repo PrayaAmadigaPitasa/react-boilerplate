83734fbccfd573eaa1f8b8904d47eda83366a6b0
Praya Amadiga Pitasa <prayaamadigapitasa@gmail.com>
2021-03-05 10:51:40 +0700

    chore(core): implement font structure

---
+++ src/consts/fonts/FontConfig.ts
@@ -0,0 +1,8 @@
+import { FontFamilyMap } from "../../types";
+
+export const FONT_FAMILY_MAP: FontFamilyMap = {
+  normal: "",
+  medium: "",
+  semi_bold: "",
+  bold: "",
+};
---
+++ src/types/fonts/FontType.ts
@@ -0,0 +1,5 @@
+export type FontSizeType = "xxs" | "xs" | "s" | "m" | "l" | "xl" | "xxl";
+
+export type FontWeightType = "normal" | "medium" | "semi_bold" | "bold";
+
+export type FontFamilyMap = { [T in FontWeightType]: string };
