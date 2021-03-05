49bfb128f6872a63d6cb88ef44977f869f70f668
Praya Amadiga Pitasa <prayaamadigapitasa@gmail.com>
2021-03-05 15:20:49 +0700

    chore(core): add dark theme

--- src/themes/ThemeMap.ts
+++ src/themes/ThemeMap.ts
@@ -1,7 +1,7 @@
-import { THEME_LIGHT } from "./types";
+import { THEME_LIGHT, THEME_DARK } from "./types";
 import { ThemeMap } from "./Theme";
 
 export const THEME_MAP: ThemeMap = {
   light: THEME_LIGHT,
-  dark: THEME_LIGHT,
+  dark: THEME_DARK,
 };
--- src/themes/types/index.ts
+++ src/themes/types/index.ts
@@ -1,1 +1,2 @@
 export * from "./ThemeLight";
+export * from "./ThemeBlack";
