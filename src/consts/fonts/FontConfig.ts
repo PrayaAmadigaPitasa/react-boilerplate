5916444be6705f38694c5234841ecce879f1c671
Praya Amadiga Pitasa <prayaamadigapitasa@gmail.com>
2021-03-05 11:51:33 +0700

    chore(core): add more fonts

--- src/consts/fonts/FontConfig.ts
+++ src/consts/fonts/FontConfig.ts
@@ -1,8 +1,8 @@
 import { FontFamilyMap } from "../../types";
 
 export const FONT_FAMILY_MAP: FontFamilyMap = {
-  regular: "",
-  medium: "",
-  semi_bold: "",
-  bold: "",
+  regular: "Raleway-Regular",
+  medium: "Raleway-Medium",
+  semi_bold: "Raleway-SemiBold",
+  bold: "Raleway-Bold",
 };
