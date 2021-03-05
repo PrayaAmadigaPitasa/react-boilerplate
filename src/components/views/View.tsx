626541f74d38b334abf623417d42ee3fa1e34281
Praya Amadiga Pitasa <prayaamadigapitasa@gmail.com>
2021-03-05 11:59:58 +0700

    chore(core): inherit view component

--- src/components/views/View.tsx
+++ src/components/views/View.tsx
@@ -1,7 +1,9 @@
-export default function View() {
-  return (
-    <>
-      <></>
-    </>
-  );
+export interface ViewProps
+  extends React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+  > {}
+
+export default function View({ ...props }: ViewProps) {
+  return <div {...props} />;
 }
