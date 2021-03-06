72d79465480e8f955486e0d4b73ff8fc65a7b1c2
Praya Amadiga Pitasa <prayaamadigapitasa@gmail.com>
2021-03-06 20:53:45 +0700

    chore(core): add icons

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import "./App.css";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { initTranslation } from "./i18n";
+import App from "./App";
+import "./index.css";
+import "./App.css";
+import "./fontello.css";
 
 initTranslation();
 
