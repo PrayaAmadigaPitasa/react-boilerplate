4052a54d7a0af4d585a6dce8b55dc9006d1cd167
Praya Amadiga Pitasa <prayaamadigapitasa@gmail.com>
2021-03-06 21:49:11 +0700

    refactor(core): metrics

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import reportWebVitals from "./reportWebVitals";
 import { initTranslation } from "./i18n";
+import { reportWebVitals } from "./metrics";
+import App from "./App";
 import "./assets/css/index.css";
 import "./assets/css/main.css";
 import "./assets/css/fontello.css";
-import App from "./App";
 
 initTranslation();
 
@@ -19,4 +19,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
+reportWebVitals();
