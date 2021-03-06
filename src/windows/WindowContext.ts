07c25fb3e3cc00a22b1dde4d16d9bd6f00656fd6
Praya Amadiga Pitasa <prayaamadigapitasa@gmail.com>
2021-03-06 18:52:04 +0700

    chore(core): add window context

---
+++ src/windows/WindowContext.ts
@@ -0,0 +1,7 @@
+import { createContext } from "react";
+import { Layout } from "../types";
+
+export const WindowLayoutContext = createContext<Layout>({
+  width: window.innerWidth,
+  height: window.innerHeight,
+});
---
+++ src/windows/WindowProvider.tsx
@@ -0,0 +1,31 @@
+import { ReactNode, useCallback, useEffect, useState } from "react";
+import { Layout } from "../types";
+import { WindowLayoutContext } from "./WindowContext";
+
+export interface WindowProviderProps {
+  children: ReactNode;
+}
+
+export default function WindowProvider({ children }: WindowProviderProps) {
+  const [layout, setLayout] = useState<Layout>({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  const handleResize = useCallback(
+    () => setLayout({ width: window.innerWidth, height: window.innerHeight }),
+    []
+  );
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, [handleResize]);
+
+  return (
+    <WindowLayoutContext.Provider value={layout}>
+      {children}
+    </WindowLayoutContext.Provider>
+  );
+}
