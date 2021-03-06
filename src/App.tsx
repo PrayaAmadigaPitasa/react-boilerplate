2c165225fea5683bdf3ec16dc53e94ed6aa2e730
Praya Amadiga Pitasa <prayaamadigapitasa@gmail.com>
2021-03-06 08:17:13 +0700

    chore(core): implement translation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import { initTranslation } from "./i18n";
 import { NavigationStack } from "./navigations";
 import { ThemeProvider } from "./themes";
+
+initTranslation();
 
 export default function App() {
   return (
--- src/i18n/Translation.ts
+++ src/i18n/Translation.ts
@@ -1,3 +1,16 @@
-import i18next from "i18next";
+import i18next, { TOptions } from "i18next";
+import { TranslationKey } from "./TranslationKey";
+import { TRANSLATION_EN, TRANSLATION_ID } from "./translations";
 
-i18next.init({ lng: "en", resources: { en: {} } });
+export function initTranslation() {
+  if (!i18next.isInitialized) {
+    i18next.init({
+      lng: "en",
+      resources: { en: TRANSLATION_EN, id: TRANSLATION_ID },
+    });
+  }
+}
+
+export function translate(key: TranslationKey, options?: TOptions | string) {
+  return i18next.t(key, options);
+}
