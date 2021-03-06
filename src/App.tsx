58576ca075f4552bebc22ab6bdd4628f1fa9e1c6
Praya Amadiga Pitasa <prayaamadigapitasa@gmail.com>
2021-03-06 18:59:05 +0700

    chore(core): implement window layout provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { initTranslation } from "./i18n";
 import { NavigationStack } from "./navigations";
 import { ThemeProvider } from "./themes";
+import { WindowProvider } from "./windows";
 
 initTranslation();
 
 export default function App() {
   return (
-    <ThemeProvider>
-      <NavigationStack />
-    </ThemeProvider>
+    <WindowProvider>
+      <ThemeProvider>
+        <NavigationStack />
+      </ThemeProvider>
+    </WindowProvider>
   );
 }
--- src/windows/index.ts
+++ src/windows/index.ts
@@ -1,3 +1,5 @@
 export * from "./WindowContext";
 export * from "./WindowHooks";
 export * from "./WindowProvider";
+
+export { default as WindowProvider } from "./WindowProvider";
