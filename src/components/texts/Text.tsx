0e125dc93189904bf4d1c1a31cb8c3c5de1a3513
Praya Amadiga Pitasa <prayaamadigapitasa@gmail.com>
2021-03-07 10:20:20 +0700

    chore(core): implement aliases on text

--- src/components/texts/Text.tsx
+++ src/components/texts/Text.tsx
@@ -1,6 +1,6 @@
 import { DetailedHTMLProps, HTMLAttributes } from "react";
 import { FONT_FAMILY_MAP } from "@constants";
-import { CSSClassNameType, FontWeightType } from "../../types";
+import { CSSClassNameType, FontWeightType } from "@types";
 
 export interface TextProps
   extends DetailedHTMLProps<
